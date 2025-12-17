27326bf193517fc60cc475f45e4c66157689dcdd
Marko Ilic <marezii97@gmail.com>
2025-12-17 22:56:21 +0100

    Add run_after apply changes script

---
+++ run_after_echo_instructions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "------------------------------------------------------------------------"
+echo "✅ Chezmoi apply complete!"
+echo "🔁 To see changes immediately, run: source ~/.zshrc"
+echo "------------------------------------------------------------------------"
